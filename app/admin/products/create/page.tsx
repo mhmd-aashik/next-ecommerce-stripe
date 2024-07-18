c6e5cac83cb8c31f27e996a3d2f6d79b83c0639d
mhmd-aashik <aashikdevelop@gmail.com>
2024-07-19 05:11:05 +0530

    create product supabe bucket image

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { faker } from "@faker-js/faker";
+import CheckboxInput from "@/components/form/CheckBoxInput";
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
+import ImageInput from "@/components/form/ImageInputContainer";
+import PriceInput from "@/components/form/PriceInput";
+import { SubmitButton } from "@/components/form/SubmitButton";
+import TextAreaInput from "@/components/form/TextAreaInput";
+import { createProductAction } from "@/lib/actions/fetchProduct.action";
 
-const page = () => {
+function CreateProductPage() {
+  const name = faker.commerce.productName();
+  const company = faker.company.name();
+  const description = faker.lorem.paragraph({ min: 10, max: 12 });
   return (
-    <div>page</div>
-  )
-}
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">create product</h1>
+      <div className="border p-8 rounded-md">
+        <FormContainer action={createProductAction}>
+          <div className="grid gap-4 md:grid-cols-2 my-4">
+            <FormInput
+              type="text"
+              name="name"
+              label="product name"
+              defaultValue={name}
+            />
+            <FormInput
+              type="text"
+              name="company"
+              label="company"
+              defaultValue={company}
+            />
+            <PriceInput />
+            <ImageInput />
+          </div>
+          <TextAreaInput
+            name="description"
+            labelText="product description"
+            defaultValue={description}
+          />
+          <div className="mt-6">
+            <CheckboxInput name="featured" label="featured" />
+          </div>
 
-export default page
+          <SubmitButton text="Create Product" className="mt-8" />
+        </FormContainer>
+      </div>
+    </section>
+  );
+}
+export default CreateProductPage;
